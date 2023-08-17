d02018032b9b9f9b050a045772d68387f06efb81
RifaatAA <rifaat.alassad@gmail.com>
2023-08-17 16:01:41 +0200

    new file

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,33 @@
+// TODO: Create 4-String variables to introduce develop your story.
+let greetings = "This is Gotham city";
+let name = "Batman";
+let title = "Gotham's protecter";
+let worldName = 'Gotham';
+
+
+
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+let age = 25;
+let year = 2042;
+let century = 21;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+let weaponsUsed = ['Batrang', 'Grapler', 'Remote Hacking Device', 'Utility Belt'];
+
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+let hasFavWeapon = true;
+
+
+// TODO: Print your story to the console.
+console.log(greetings + " My name is " + name + ".");
+console.log("I'm " + title);
+console.log("As the leader of " + worldName + ", I use different weapons.");
+console.log("Things works differently in my world.");
+console.log("For example, crime rates have been increasing for the past " + age + " years.");
+console.log("In my world, you are in year " + year + ", the " + century + "st century." + "\n");
+console.log("Does anyone have any questions?");
+console.log("Me: Do you have a favorite weapon?");
+console.log("Batman: " + hasFavWeapon + ", my favorite weapon is the " + weaponsUsed[2] + "." + "\n");
+console.log("You are free to explore my world and return to Earth after you help me take down the Joker!");
